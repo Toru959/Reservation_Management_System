b1b2561cdc361f9209c837df4cef1e20d27f0915
Toru959 <80747436+Toru959@users.noreply.github.com>
2023-05-23 10:32:20 +0800

    event計算

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -12,6 +12,8 @@ class Calendar extends Component
     public $currentDate;
     public $currentWeek;
     public $day;
+    public $checkDay;
+    public $dayOfWeek;
     public $sevenDaysLater;
     public $events;
 
@@ -28,8 +30,16 @@ class Calendar extends Component
 
         for($i = 0; $i < 7; $i++){
             $this->day = CarbonImmutable::today()->addDays($i)->format('m/d');
+            $this->checkDay = CarbonImmutable::today()->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::today()->addDays($i)->dayName;
 
-            array_push($this->currentWeek, $this->day);
+
+            array_push($this->currentWeek, 
+            [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek,
+            ]);
         }
         // dd($this->currentWeek);
 
@@ -48,10 +58,16 @@ class Calendar extends Component
 
         for($i = 0; $i < 7; $i++)
         {
-            $this->day = CarbonImmutable::parse($this->currentDate)->addDays($i)
-            ->format('m/d'); // parseでcarbonインスタンスに変更後、日付を加算
+            $this->day = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('m/d'); // parseでcarbonインスタンスに変更後、日付を加算
+            $this->checkDay = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::parse($this->currentDate)->addDays($i)->dayName;
 
-            array_push($this->currentWeek, $this->day);
+            array_push($this->currentWeek, 
+            [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek,
+            ]);
         }
     }
 
